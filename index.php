098ce6a7418494e6a3f9d422265eb43a373f6f37
asrianor <33111018+asrianor@users.noreply.github.com>
2018-04-05 15:21:40 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?
+//ini adalah comment
+/*ini juga comment karena itu tidak ditampilkan*/
+echo “Hello World…!!!”;
+echo “<br />”;
+print “Hello Again World…!!!”;
+print “<br /><br />”;
+
+echo (“echo() bisa digunakan dengan atau tanpa kurung buka-tutup seperti pada contoh<br />”);
+print (“demikian juga dengan print()<br />”);
+?>
+echo “Hello World…!!!”;
